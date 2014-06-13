12ad23373187bbb04784777dc46b6a19f2422ea9
ChubSsassino <info@ronisserles.com>
2014-06-13 14:35:13 +0300

    second commit

--- GitTest/src/com/example/gittest/MainActivity.java
+++ GitTest/src/com/example/gittest/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.gittest;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.webkit.ConsoleMessage;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +12,8 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		TextView tv = (TextView)findViewById(R.id.text);
+		tv.setText("text");
 	}
 
 	@Override
