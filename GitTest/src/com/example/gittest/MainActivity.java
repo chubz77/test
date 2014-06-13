cee8d22d613321391fdc8baf5447fdeccf4f9251
Tardis <info@ronisserles.com>
2014-06-13 16:06:52 +0300

    jhgfjhgfjhg

--- GitTest/src/com/example/gittest/MainActivity.java
+++ GitTest/src/com/example/gittest/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends Activity {
 		setContentView(R.layout.activity_main);
 		TextView tv = (TextView)findViewById(R.id.text);
 		tv.setText("text");
+		//jhgfjhgfjhgf
 	}
 
 	@Override
